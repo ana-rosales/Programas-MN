0665deb2080a25169cba55fdef064eec5c88e7a6
ana-rosales <anapaula161103@gmail.com>
2022-12-12 01:10:44 -0600

    FEAT: Comienzos con javafx, errores en css y alerta

--- src/fes/aragon/inicio/BiseccionInicio.java
+++ src/fes/aragon/inicio/BiseccionInicio.java
@@ -3,69 +3,28 @@ package fes.aragon.inicio;
 import java.util.Scanner;
 
 import fes.aragon.modelo.Biseccion;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class BiseccionInicio {
-	@SuppressWarnings("resource")
-	public static void main(String[] args) {
-		int errno = 0;
-		Biseccion current;
-		Double sup, inf, err;
-		Scanner usuFun, usuSup, usuInf, usuErr;
-		String expresion, txtSup, txtInf, txtErr;
-		
-		System.out.println("\nIntroduzca funcion:");
-		usuFun = new Scanner(System.in);
-		expresion = usuFun.next();
-		System.out.println("Introduzca límite superior:");
-		usuSup = new Scanner(System.in);
-		txtSup = usuSup.next();
-		System.out.println("Introduzca límite inferior:");
-		usuInf = new Scanner(System.in);
-		txtInf = usuInf.next();
-		System.out.println("Introduzca margen de error:");
-		usuErr = new Scanner(System.in);
-		txtErr = usuErr.next();
-		
-		if (expresion.equals("") || txtSup.equals("") || txtInf.equals("") || txtErr.equals("")) {
-			System.out.println("Campos vacíos.");
-			errno++;
-			return;
-		}
-
-		sup = Double.parseDouble(txtSup);
-		inf = Double.parseDouble(txtInf);
-		err = Double.parseDouble(txtErr);
-
-		current = new Biseccion(expresion, inf, sup);
-		current.acomodarLimites();
-
+public class BiseccionInicio extends Application{
+	@Override
+	public void start(Stage primaryStage) {
 		try {
-			if (current.f(inf) * current.f(sup) < 0) {
-				do {
-					current.setRaiz();
-					current.setProducto();
-					if (current.prod().get() < 0) {
-						current.sup().set(current.raiz().get());
-					} else if (current.prod().get() > 0) {
-						current.inf().set(current.raiz().get());
-					}
-					current.setErrRaiz();
-					current.iterar();
-				} while (current.errRaiz().get() >= err);
-			} else {
-				System.out.println("Entre los limites indicados, no existe raiz.");
-				errno++;
-			}
-		} catch (Exception e) {
-			System.out.println(current.error().get());
-			errno++;
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/fes/aragon/ui/fxml/Biseccion.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Biseccion");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
-
-		if (errno == 0) {
-			System.out.println(current.raiz().get());
-			System.out.println(current.iteraciones().get() + "");
-		} else {
-			errno = 0;
-		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
 	}
 }
